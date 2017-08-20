032268398b150aeee17d1de7628abb39b68458b1
Nisse <3332423+NumeriusNegidius@users.noreply.github.com>
2017-08-20 14:31:33 +0200

    Added submenus

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-// DEFINE FOLDER IN WHICH SEARCHABLE BOOKMARKS SHOULD BE LOCATED:
-var folderName = "Searches"
+// Define root folder for searches
+const FOLDER_NAME = "Searches"
 
-// ERROR LOGGING:
+// Error logging
 function onCreated(n) {
   if (browser.runtime.lastError) {
     console.log(`Error: ${browser.runtime.lastError}`);
@@ -12,48 +12,79 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
-// PARSE THROUGH ALL BOOKMARKS IN TREE AND MAKE FIRE populateContextMenu FOR EACH:
-function listBookmarksInTree(bookmarkItem) {
-  if (bookmarkItem.url) {
-//    console.log(bookmarkItem.title + " - " + bookmarkItem.url);
-    populateContextMenu(bookmarkItem.id, bookmarkItem.title, bookmarkItem.url);
-  }
-  if (bookmarkItem.children) {
-    for (child of bookmarkItem.children) {
-      listBookmarksInTree(child);
-    }
-  }
-}
-
-// DON'T KNOW WHAT IT DOES:
-function logSubTree(bookmarkItems) {
-  listBookmarksInTree(bookmarkItems[0], 0);
-}
-
-// GET ID OF folderName AND PASS THROUGH logSubTree:
-var searching = browser.bookmarks.search({title: folderName});
+// Get ID of FOLDER_NAME and the object and pass everything through logSubTree:
+var searching = browser.bookmarks.search({title: FOLDER_NAME});
 searching.then((bookmarks) => {
-  folderID = bookmarks[0];
-//  console.log(folderID.id);
-
-  var subTreeID = folderID.id;
+  subTreeID = bookmarks[0].id;
 
   var gettingSubTree = browser.bookmarks.getSubTree(subTreeID);
   gettingSubTree.then(logSubTree, onError);
 });
 
-// MAKE THE CONTEXT MENU
-function populateContextMenu(id, title, url) {
-  browser.contextMenus.create({
-    id: url,
-    title: title,
-    contexts: ["selection"],
-    onclick: goTo
-  }, onCreated());
+function logSubTree(bookmarkItems) {
+  var subTreeID = bookmarkItems[0].id;
+//  console.log("Root: " + subTreeID);
+
+  listBookmarksInTree(bookmarkItems[0], subTreeID);
 }
 
-// REPLACE THE BROWSER STANDARD %s FOR KEYWORD SEARCHES WITH
-// THE SELECTED TEXT ON THE PAGE AND GO TO URL
+// Parse through all bookmarks in tree and fire populateContextMenu for each:
+function listBookmarksInTree(bookmarkItem, subTreeID) {
+//  console.log("ID: " + bookmarkItem.id + ". Title: " + bookmarkItem.title + ". Parent: " + bookmarkItem.parentId + ". STID: " + subTreeID);
+  populateContextMenu(bookmarkItem.id, bookmarkItem.title, bookmarkItem.url, bookmarkItem.parentId, subTreeID);
+
+  if (bookmarkItem.children) {
+    for (child of bookmarkItem.children) {
+      listBookmarksInTree(child, subTreeID);
+    }
+  }
+}
+
+// Make the context menu
+function populateContextMenu(id, title, url, parent, subTreeID) {
+
+  //Parse everything except root folder
+  if (id != subTreeID) {
+
+    // These are the folders
+    if (!url) {
+//      console.log("Make folder: " + id)
+      browser.contextMenus.create({
+        id: id,
+        title: title,
+        contexts: ["selection"]
+      }, onCreated());
+    }
+    else {
+
+      // These are the bookmarks in subfolders
+      if (parent != subTreeID) {
+//        console.log("Make link: " + id + ". P: " + parent + ". STID: " + subTreeID)
+        browser.contextMenus.create({
+          parentId: parent,
+          id: url,
+          title: title,
+          contexts: ["selection"],
+          onclick: goTo
+        }, onCreated());
+      }
+      // These are the bookmarks in the root folder
+      else {
+//        console.log("Make link: " + id + ". P: " + parent + ". STID: " + subTreeID)
+        browser.contextMenus.create({
+          id: url,
+          title: title,
+          contexts: ["selection"],
+          onclick: goTo
+        }, onCreated());
+      }
+
+    }
+  }
+}
+
+// replace the browser standard %s for keyword searches with
+// the selected text on the page and go to url
 function goTo(info) {
 //  console.log("Selection " + info.selectionText + " and URL " + info.menuItemId);
   browser.tabs.create({
