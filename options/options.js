aa99dd27b8ec5dd2e43ec78c3393a286ccdf3187
Nisse <3332423+NumeriusNegidius@users.noreply.github.com>
2017-10-18 00:20:25 +0200

    removed innerHTML

--- options/options.js
+++ options/options.js
@@ -37,8 +37,8 @@ function localize() {
   let getNode = document.getElementsByClassName("l10n");
   for (let i = 0; i < getNode.length; i++) {
     let node = getNode[i];
-    let msg = node.innerHTML;
-    node.innerHTML = browser.i18n.getMessage(msg);
+    let msg = node.textContent;
+    node.firstChild.nodeValue = browser.i18n.getMessage(msg);
   }
 }
 
